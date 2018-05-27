bbfaa25f184fd4d70c9ecd49563682c139be41bd
Keven <tv2014@126.com>
2018-05-27 21:46:13 +0800

    更新基础教程学习笔记

---
+++ test_everyday/180527(自动添加标签)/__init__.bat
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Keven at 2018/5/27
+
+
+python markup.py < test_input.txt > text_output.html
+
+python simple_markup.py < test_input.txt > text_output1.html
